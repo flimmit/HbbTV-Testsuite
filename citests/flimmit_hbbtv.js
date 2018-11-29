3088ff1e79e00ec77818b1194a7435b03e87c888
Halit TIRYAKI <halit@flimmit.com>
2018-11-29 11:35:44 +0100

    bugfix

--- citests/flimmit_hbbtv.js
+++ citests/flimmit_hbbtv.js
@@ -93,7 +93,7 @@ function getDRMIds(){
         for(v in capDRM){
             if (typeof capDRM[i] == 'object') {
                 var sysId = capDRM[i].getAttribute('DRMSystemID');
-                if (typeof sysId == 'string' && !sysId.lastIndexOf(':0') != sysId.length -2){
+                if (typeof sysId == 'string' && sysId.lastIndexOf(':0') != sysId.length -2){
                     DRMIds.push(capDRM[i].getAttribute('DRMSystemID'));
                 }
             }
